12b1e834c6dbd09d04e788400e004d379906a701
ryaneof <ryaneof@gmail.com>
2014-08-20 01:18:42 +0800

    remove gulp, use mocha only

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-
-gulp.task('default', function () {
-  return gulp.src('./test/*.js', { read: false })
-            .pipe(mocha({ 
-              timeout: 20000 
-            }));
-});
